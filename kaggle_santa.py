1ec5e8f49f1c7fb5a8a64de1ab3101704267de12
hesam_ubuntu <saloot@gmail.com>
2019-12-08 11:32:12 +0330

    working on message passing algorithm

--- kaggle_santa.py
+++ kaggle_santa.py
@@ -1,12 +1,49 @@
 from datetime import datetime, timedelta
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import csv
 import pytz
 
+def calculate_cost(choice_no: int,no_people: int):
+    """
+    This function calculates the cost of assigning a family to
+    a given day, accounting for their preferred schedule as well
+    as the family size.
 
-# TODO: Complete this function with -1 being equivalent to the "others" choice
-def calculate_cost(choice_no):
+    params:
+    choice_no: indicated the desirability of the speicifed day 
+                according to the families schedule, 0 being the
+                most desirable
+    no_people: Number of the people in the family
+
+    output params:
+    cost: the cost of assigning the family to the day specified 
+          by the choice number
+    """
+
+    if choice_no == 0:
+        cost = 0
+    elif choice_no == 1:
+        cost = 50
+    elif choice_no == 2:
+        cost = 50 + 9 * no_people
+    elif choice_no == 3:
+        cost = 100 + 9 * no_people
+    elif choice_no == 4:
+        cost = 200 + 9 * no_people
+    elif choice_no == 5:
+        cost = 200 + 18 * no_people
+    elif choice_no == 6:
+        cost = 300 + 18 * no_people
+    elif choice_no == 7:
+        cost = 300 + 36 * no_people
+    elif choice_no == 8:
+        cost = 400 + 36 * no_people
+    elif choice_no == 9:
+        cost = 500 + 36 * no_people + 199 * no_people
+    else:
+        cost = 500 + 36 * no_people + 398 * no_people
 
     return cost
 
@@ -17,30 +54,30 @@ file_name = './Data/family_data.csv'
 f = open(file_name,'r')
 reader = csv.reader(f)
 data_ub = list(reader)
-no_families = len(data_ub)
+no_families = len(data_ub)-1
 no_days = 100 #max(sample_submission[:,1])
 
 family_data = np.zeros([no_families,12])
 for itr in range(1,len(data_ub)):
     item = data_ub[itr]
-    family_data[itr,:] = item
-
+    family_data[itr-1,:] = item
+family_data = family_data.astype(int)
 
 # Create cost matrix
 C = np.zeros([no_families,no_days])
-for in range(0,no_families):
+for i in range(0,no_families):
     choices = family_data[i,1:-1]
     no_people = family_data[i,-1]
     family_id = family_data[i,0]
 
     for itr in range(0,len(choices)):
         j = choices[itr]
-        C[i,j] = calculate_cost(itr)
+        C[i,j-1] = calculate_cost(itr,no_people)
 
-    for j in range(0,no_days):
+    for j in range(1,no_days+1):
         if j in choices:
             continue
         else:
-            C[i,j] = calculate_cost(-1)
+            C[i,j-1] = calculate_cost(-1,no_people)
 
     
---
+++ message_passing.py
@@ -0,0 +1,113 @@
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+import pytz
+
+BIG_COST = 1000000
+min_occupancy = 125
+max_occupancy = 300
+
+def calculate_total_cost(assignment_matrix):
+    """
+    This function calculates the total cost of assigning families
+    a given day, accounting for their preferred schedule as well
+    as the family size.
+
+    params:
+    assignment_matrix: a N*D assignment matrix
+
+    output params:
+    cost: the cost of assigning the families to the days
+    """
+
+    cost = 0
+    no_families = assignment_matrix.shape[0]
+    no_days = assignment_matrix.shape[1]
+    
+
+    for i in range(0,no_families):
+        choices = family_data[i,1:-1]
+        no_people = family_data[i,-1]
+        d = np.nonzero(assignment_matrix[i,:])[0][0]
+        try:
+            choice = list(choices).index(d+1)
+        except:
+            choice = -1
+
+        cost += calculate_cost(choice,no_people)
+
+    occupancy_count = sum(assignment_matrix)
+    for j in range(0,len(occupancy_count)):
+        Nd = occupancy_count[j] 
+        
+        Nd1 = occupancy_count[min(j+1,no_days-1)] 
+        cost += pow(Nd,0.5 + abs(Nd-Nd1)/50.) * max(Nd-125,0)/400.
+
+        if Nd < min_occupancy or Nd > max_occupancy:
+            cost += BIG_COST
+
+    return cost
+
+
+cutoff_orig = 10
+max_no_optimization_itrs = 100
+choices_inds = np.zeros([no_families,1]).astype(int)
+for itr in range(0,max_no_optimization_itrs):
+    ForwardMatrix = np.zeros([no_families,no_days]).astype(int)
+    previous_choices_inds = choices_inds
+    choices_inds = np.zeros([no_families,1]).astype(int)
+
+    cutoff = cutoff_orig/(1.+itr)
+    # Forward step: people send their schedule to the constraints
+    for i in range(0,no_families):
+        choices = family_data[i,1:-1]
+        no_people = family_data[i,-1]
+        
+        # Check the messages comming from neighbors
+        feedback = BackwardMatrix[i,:]
+
+        # Adjust the choice based on the feedback coming from constraints
+        #if itr > 0:
+        #    pdb.set_trace()
+        overall_feedback = sum(feedback) #sum(feedback > 0) - sum(feedback < 0) 
+
+        if overall_feedback > cutoff:
+            new_ind = previous_choices_inds[i] #min(previous_choices_inds[i]+1,9)
+        elif overall_feedback < -cutoff:
+            new_ind = min(previous_choices_inds[i]+1,9) #max(previous_choices_inds[i]-1,0)
+        else:
+            new_ind = previous_choices_inds[i]
+        
+        choice = choices[new_ind]
+        choices_inds[i] = new_ind
+        ForwardMatrix[i,choice-1] = no_people
+    print(sum(sum(ForwardMatrix))/100.)
+
+    print(calculate_total_cost(ForwardMatrix))
+    # Backward step
+    BackwardMatrix = np.zeros([no_families,no_days]).astype(int)
+    for j in range(0,no_days):
+        
+        # Check the messages comming from neighbors
+        feedback = ForwardMatrix[:,j]
+
+        # Adjust the choice based on the feedback coming from constraints
+        m = 0
+        if sum(feedback) > max_occupancy:
+            m = max_occupancy - sum(feedback)
+        elif sum(feedback) < min_occupancy:
+            m = min_occupancy - sum(feedback)
+        
+        for i in np.nonzero(feedback)[0]:
+            if m == 0:
+                no_people = family_data[i,-1]
+                choices = family_data[i,1:-1]
+                try:
+                    choice = list(choices).index(j+1)
+                except:
+                    choice = -1
+                m = -calculate_cost(choice,no_people)
+            BackwardMatrix[i,j] = m
+
